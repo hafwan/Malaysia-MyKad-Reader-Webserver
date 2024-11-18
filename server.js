a1c9091992a9c92635b7d10bc211fa2efcbae658
hafwan <hafwan@iium.edu.my>
2024-11-18 08:41:19 +0800

    Signed-off-by: hafwan <hafwan@iium.edu.my>

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { extractMyKad } = require("./utils/mykad");
+const { extractMyKid } = require("./utils/mykid");
 var cors = require('cors')
 var app = express()
 const port = 9090;
@@ -25,6 +26,44 @@ app.post("/api/smartcard/mykad", async (req, res) => {
 
     console.info({
       status: 'MyKad berjaya dibaca.',
+      nama: data?.name
+    });
+
+    res.status(200).json(data)
+
+  } catch (error) {
+
+    console.error({
+      status: 'Kad gagal dibaca.',
+      detail: error.message 
+    });
+
+    res.status(404).json({ 
+      "status": 404,
+      "title":"Tidak berjaya.",
+      "detail": error.message,  
+    });
+  }
+});
+
+app.post("/api/smartcard/mykid", async (req, res) => {
+  
+  // console information
+  console.clear();
+  console.info(`
+    MyKidReader Webserver\n
+    
+    This program need to run to read MyKid from
+    attached card reader and bound to:
+    POST localhost:9090/api/smartcard/mykid
+    -------------------------------------------\n
+  `);
+
+  try {
+    let data = await extractMyKid();
+
+    console.info({
+      status: 'MyKid berjaya dibaca.',
       nama: data?.name
     });
 
